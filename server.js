fb3264fe782408cdfddf57f8c5498b3add66f088
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-02-02 18:34:26 +0530

    server.js: added proper null checks

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const net = require('net');
-let myMap = {}
+let myMap = null
 const server = net.createServer(conn => {
     console.log("Client Connected...");
     const clientAddress = conn.address().address;
@@ -7,23 +7,39 @@ const server = net.createServer(conn => {
 
     conn.on('data', data => {
         // Handle incoming data
-        let dataString = data.toString().split(' ');
-
+        let dataString = data.toString().trim().split(' ');
+        console.log(dataString);
         let i = 0;
         while (i < dataString.length) {
             let key, value;
-            if (dataString[i] === "PUT") {
+            if (dataString[i] === "put") {
                 key = dataString[i + 1];
                 value = dataString[i + 2];
+                if(myMap === null) {
+                    myMap = {};
+                    myMap[clientAddress] = {};
+                }
                 myMap[clientAddress][key]=value;
                 i += 3;
-                continue;
+                conn.write("Data saved successfully");
+                
             }
-            if (dataString[i] === "GET") {
+            if (dataString[i] === "get") {
+                if(myMap === null) {
+                    conn.write("No data avaiable");
+                    return;
+                }
+                if(!myMap[clientAddress]) {
+                    conn.write("No data avaiable");
+                    return;
+                }
+                
                 key = dataString[i + 1];
+                console.log(myMap[clientAddress][key]);
                 value = myMap[clientAddress][key];
                 i += 2;
                 conn.write(value);
+                
             }
         }
     });
@@ -34,7 +50,7 @@ const server = net.createServer(conn => {
 });
 
 
-const ipAddress = "192.168.0.185"; 
+const ipAddress = "192.168.0.107"; //use your own ip here 
 const port = 9000;
 
 server.listen(port, ipAddress, () => {
